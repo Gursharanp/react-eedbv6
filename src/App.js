5b61248264e1dda4400b525266129ac56fbfcd18
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-01-17 11:08:08 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./style.css";
+import React, {useState,useEffect} from 'react';
 import Comp from "./Comp.js"; 
 export default function App() {
+  const[flag,setCount]=useState(true));
   return (
     <div>
       
