e2321d7b167d68d2800a74076f9560e0ee48ef37
Gursharanp <gursharanpreetsinghkang2@gmail.com>
2022-01-17 11:03:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-eedbv6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-eedbv6)
---
+++ src/Comp.js
@@ -0,0 +1,34 @@
+import React, {useState,useEffect} from 'react';
+export default function Comp(){
+  const[count,setCount]=useState(0);
+//
+useEffect(()=>{
+  console.log('render');
+});
+useEffect(()=>{
+console.log("runs only once")
+},[]);
+
+useEffect(()=>{
+  console.log("runs when array state changes")
+  },[count]);
+
+  useEffect( ()=>{
+    return()=>{
+      console.log("unmount");
+    }
+  },[] ;)
+
+
+
+
+return(
+<div>
+  <button onClick={()=> setCount(count -1)}>-</button>
+  {count}
+  <button onClick={()=> setCount(count +1)}>+</button>
+
+</div>
+
+);
+}
